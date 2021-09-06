93f51d639f67657bb53d929e9a3e9c535a8d692e
Murali Krishna <muralikrishna@Muralis-MacBook-Air.local>
2021-09-07 00:49:49 +0530

    Insertion Sort :7/9/21

--- Algorithms/InsertionSort.java
+++ Algorithms/InsertionSort.java
@@ -5,7 +5,6 @@ class InsertionSort implements Sort{
 	InsertionSort(){}
 	
 	public int[] sort(int[] array){
-
 		args = array;
 		for(int i =0; i< args.length; i++){
 			for (int j = i; j>0; j--)
@@ -15,26 +14,17 @@ class InsertionSort implements Sort{
 				else break;
 		}
 		return args;
-
 	}
-
 
 	// Checks a is less than b
-
 	public Boolean isLess(int a , int b) {
-
 		return a<b;
-
 	}
 
-
 	// i, j are indexes
-
 	public void swap(int i, int min){
-
 		int temp =args[min];
 		args[min] = args[i];
 		args[i] = temp;
-
 	}
 }
