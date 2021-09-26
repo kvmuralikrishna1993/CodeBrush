3bdca255caedc48dac32decc2c07650a866bbd78
Murali Krishna <kvmuralikrishna1993@msitprogram.net>
2021-09-26 23:28:29 +0530

    pending properties

---
+++ Problems/BinaryTree/BinaryTree.java
@@ -0,0 +1,31 @@
+class BinaryTree{
+
+	BinaryTree(){}
+
+	Boolean contains(TreeNode node){
+
+		return false;
+
+	}
+
+	int getMax(TreeNode node){
+
+		return -1;
+
+	}
+
+	int maxDepth(TreeNode node){
+
+		return -1;
+	}
+
+	int maxWidth(TreeNode node){
+		
+		return -1;
+	}
+
+	Boolean isSymmentric(TreeNode node){
+
+		return false;
+	}
+}
